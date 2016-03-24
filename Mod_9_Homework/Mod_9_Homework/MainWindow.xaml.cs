99a5e94f137f2321d76aa93e3d2056639ebc2c33
rombo84fx <gmart_nez@yahoo.com.mx>
2016-03-24 14:07:34 -0600

    Module 9 assignment

---
+++ Mod_9_Homework/Mod_9_Homework/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mod_9_Homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Student> students = null;
+        int index = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.students = new List<Student>();
+        }
+
+        private void btnCreateStudent_Click(object sender, RoutedEventArgs e)
+        {
+            Student student = new Student(txtFirstName.Text, txtLastName.Text, txtCity.Text);
+            this.students.Add(student);
+            this.index = this.students.Count;
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtCity.Clear();
+            MessageBox.Show("Student created");
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.students.Count != 0)
+            {
+                if (this.index == 0)
+                {
+                    this.index = this.students.Count - 1;
+                }
+                else
+                {
+                    this.index--;
+                }
+                populateTextBoxes(this.students[this.index]);
+            }
+            else
+            {
+                MessageBox.Show("There are no students in the collection.");
+            }
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (students.Count != 0)
+            {
+                if (this.index >= this.students.Count - 1)
+                {
+                    this.index = 0;
+                }
+                else
+                {
+                    this.index++;
+                }
+                populateTextBoxes(this.students[this.index]);
+            }
+            else
+            {
+                MessageBox.Show("There are no students in the collection.");
+            }
+        }
+
+        private void populateTextBoxes(Student student)
+        {
+            txtFirstName.Text = student.FirstName;
+            txtLastName.Text = student.LastName;
+            txtCity.Text = student.City;
+        }
+    }
+}
---
+++ Mod_9_Homework/Mod_9_Homework/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_9_Homework
+{
+    class Student
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _city;
+
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public Student(string FirstName, string LastName, string City)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.City = City;
+        }
+    }
+}
